3ec13390a7d032db94242d7fe52b0f2d47af25e1
gollumeo <p.mauriello54@gmail.com>
2024-03-13 22:28:48 +0100

    init tailwind & vite

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "App.vue"
+}
+</script>
+
+<template>
+<div>
+    <h1>Hello world</h1>
+    <router-view></router-view>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,9 @@ import './bootstrap';
 import { createApp } from 'vue'; // Importing createApp correctly
 import { createPinia } from 'pinia';
 import { apiInstance } from "./api-instance.js";
+import App from './App.vue';
 
-const app = createApp();
+const app = createApp(App);
 
 const pinia = createPinia();
 
