3487b8231fa4e1776395aba656a37ed8aff508ea
gollumeo <p.mauriello54@gmail.com>
2024-03-14 07:18:16 +0100

    router

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,15 @@ import { createApp } from 'vue'; // Importing createApp correctly
 import { createPinia } from 'pinia';
 import { apiInstance } from "./api-instance.js";
 import App from './App.vue';
+import { router } from './router';
+import toast from "vue-toastification";
 
 const app = createApp(App);
 
 const pinia = createPinia();
 
+app.config.globalProperties.$api = apiInstance;
+
 app.use(pinia)
-    .use(apiInstance)
+    .use(router)
     .mount('#app');
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
